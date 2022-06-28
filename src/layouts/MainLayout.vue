46ad98676dda11a302f540ff9597e430bc640769
Snetcov Daniel <snetcovdaniel@gmail.com>
2022-06-28 15:13:16 +0300

    And so, it begins.

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,18 @@
+<template>
+  <q-layout view="lHh lpr fFf">
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+
+export default {
+  name: 'MainLayout',
+  components: {},
+  data () {
+    return {}
+  }
+}
+</script>
---
+++ src/pages/Index.vue
@@ -0,0 +1,11 @@
+<template>
+  <q-page>
+    <h1>Onboarding tracker</h1>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: "PageIndex"
+}
+</script>
