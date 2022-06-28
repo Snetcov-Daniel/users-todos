fb4ce18798d4fbbe48598e7fa061e4cbdae71910
NatanerJ <janovntn@gmail.com>
2022-06-28 22:12:14 +0300

    Added todoList of checkboxes

---
+++ src/components/TodoCheckbox.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <div class="q-gutter-sm" v-for="item in userTasks" :disabled="tasksDone.includes(item.title)"
+    :key="item.id">
+      <input type="checkbox" v-model="tasksDone" :id="item.id" :value="item.title" />
+      <label :for="item.id" >{{ item.title }}</label>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    userTasks: Array
+  },
+  data() {
+    return {
+      tasksDone: []
+    }
+  },
+};
+</script>
